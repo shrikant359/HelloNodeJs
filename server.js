2554aacf52f61dbef286701621194612c59cd56f
Shrikant Sharma <shrikantsharma359@gmail.com>
2019-04-01 15:07:33 +0530

    cmt1

---
+++ server.js
@@ -0,0 +1,43 @@
+const fs=require('fs');
+var expr=require('express');
+var hbs=require('hbs');
+
+var app=expr();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('View String','hbs');
+app.use(expr.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+	var now=new Date().toString();
+	var log=`${now}:${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log+'\n');
+	next();
+});
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt',(text)=>{
+	return text.toUpperCase();
+});
+
+
+app.get('/',(req,res)=>{
+res.render('home.hbs',{
+	pageTitle:'Home Page',
+	welcomeMessage:'Welcome to my website',
+	currentYear:new Date().getFullYear()
+     });
+});
+
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+	pageTitle:'About Page',
+	currentYear:new Date().getFullYear()
+     });
+});
+
+app.listen(3000,()=>{
+	console.log('server is upto on port 3000!');
+});
